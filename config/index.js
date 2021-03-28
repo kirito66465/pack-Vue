00ac59744953997a6be8765d1721abf8ef2d3876
kirito <1204505357@qq.com>
2021-03-28 21:49:45 +0800

    2020-03-28(4) 优化 dev 配置 Signed-off-by: kirito <1204505357@qq.com>
    Signed-off-by: kirito <1204505357@qq.com>

--- config/index.js
+++ config/index.js
@@ -13,11 +13,11 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '127.0.0.1', // can be overwritten by process.env.HOST
     port: 8082, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
-    notifyOnErrors: true,
+    notifyOnErrors: true,     // Vue-CLI 脚手架提示
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
 
